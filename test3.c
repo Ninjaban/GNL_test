25d1be4d630cd8a0b4132b112aef1e25248a3b10
Jonathan CARRA <jcarra@e1r3p6.42.fr>
2016-12-23 08:52:51 +0100

    ok

--- test3.c
+++ test3.c
@@ -6,7 +6,7 @@
 /*   By: jcarra <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 13:49:57 by jcarra            #+#    #+#             */
-/*   Updated: 2016/12/13 13:56:28 by jcarra           ###   ########.fr       */
+/*   Updated: 2016/12/23 08:52:13 by jcarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int ac, char **av)
 	if (ac == 3)
 	{
 		int fd1 = open(av[1], O_RDONLY);
-		int fd2 = open(av[1], O_RDONLY);
+		int fd2 = open(av[2], O_RDONLY);
 		while (get_next_line(fd1, &line1) == 1 || get_next_line(fd2, &line2) == 1)
 		{
 			printf("%s\n", line1);
